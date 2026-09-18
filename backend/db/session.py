ccdbb3c0f2272525951de2e429689773a753acb0
ashen-v2 <ashen.te0217@gmail.com>
2026-09-18 11:01:25 +0530

    feat: update PostgreSQL configuration to use individual database settings

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,7 +1,8 @@
 from sqlmodel import Session, create_engine
 from config.config import settings
 
-engine = create_engine(settings.database_url, echo=True)
+# engine = create_engine(settings.database_url, echo=True)
+engine = create_engine(f"postgresql://{settings.database_user}:{settings.database_password}@{settings.database_host}:5432/{settings.database_name}", echo=True)
 
 def get_session():
     with Session(engine) as session:
